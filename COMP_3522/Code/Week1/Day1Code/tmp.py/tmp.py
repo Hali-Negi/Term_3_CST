fedf0af90ab387b1443952a9ece5d0c5e1961517
Hali Imanpanah <imanpanah.haleh@gmail.com>
2026-04-26 15:21:07 -0700

    W1_Day1_code

---
+++ COMP_3522/Code/Week1/Day1Code/tmp.py/tmp.py
@@ -0,0 +1,26 @@
+# 1
+print(
+    "This is a very long string"
+    " that I wrote to help somebody"
+    " who had a question about"
+    " writing long strings in Python"
+)
+
+# # 2
+# print(
+#     (
+#         "This is a very long string"
+#         " that I wrote to help somebody"
+#         " who had a question about"
+#         " writing long strings in Python"
+#     )
+# )
+
+
+# #  3
+# print(
+#     """This is a very long string
+# 			  that I wrote to help somebody
+# 		       who had a question about
+# 			  writing long strings in Python"""
+# )
